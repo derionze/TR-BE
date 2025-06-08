b6ea5842b1a8865079b2554b0b552e99c4e15a49
ReswaraArgyaMunda <argiamc12@gmail.com>
2025-06-08 10:22:25 +0800

    modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,11 @@ import { UsersEntity } from './users/entities/users.entity';
 import { ProdukEntity } from './produk/entities/produk.entity';
 import { KeranjangUsersEntity } from './keranjang-users/entities/keranjang-users.entity';
 import { PromoEntity } from './promo/entities/promo.entity';
+import { UsersModule } from './users/users.module';
+import { KeranjangUsersModule } from './keranjang-users/keranjang-users.module';
+import { ProdukModule } from './produk/produk.module';
+import { PromoModule } from './promo/promo.module';
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
@@ -16,6 +21,11 @@ import { PromoEntity } from './promo/entities/promo.entity';
       entities: [UsersEntity, ProdukEntity, KeranjangUsersEntity, PromoEntity],
       synchronize: true,
     }),
+    UsersModule,
+    KeranjangUsersModule,
+    ProdukModule,
+    PromoModule,
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
