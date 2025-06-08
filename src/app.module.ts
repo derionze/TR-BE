7bd07039bf31e0c32f276e16eb83ce0088828a91
ReswaraArgyaMunda <argiamc12@gmail.com>
2025-06-08 10:16:31 +0800

    entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersEntity } from './users/entities/users.entity';
+import { ProdukEntity } from './produk/entities/produk.entity';
+import { KeranjangUsersEntity } from './keranjang-users/entities/keranjang-users.entity';
+import { PromoEntity } from './promo/entities/promo.entity';
+
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'database.sqlite',
-      // entities: [UsersEntity, ProdukEntity, KeranjangUsersEntity, PromoEntity],
+      entities: [UsersEntity, ProdukEntity, KeranjangUsersEntity, PromoEntity],
       synchronize: true,
     }),
   ],
